4d2ab042dc488bd25c1a8c1181fdc0a0fed82dad
soporte-dpi <soporte@dpiestrategia.com>
2026-03-11 12:26:46 +0000

    v5

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // REEMPLAZA "NOMBRE_DEL_REPO" POR EL NOMBRE REAL DE TU REPOSITORIO EN GITHUB
-  base: '/landing-formacion/', 
+  base: './', 
 })
